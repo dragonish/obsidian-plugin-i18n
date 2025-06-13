488ef0fb37aa8978de1840dc16dc096d2f11f3ec
dragonish <no.web.developer@outlook.com>
2025-06-14 00:17:58 +0800

    fix(types): fix the missing types after build

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,13 @@
 import { getLanguage } from 'obsidian';
 
+type NestedStringObject = {
+  [key: string]: string | NestedStringObject;
+};
+
+type NestedKeyOf<ObjectType extends object> = {
+  [Key in keyof ObjectType & string]: ObjectType[Key] extends object ? `${Key}` | `${Key}.${NestedKeyOf<ObjectType[Key]>}` : `${Key}`;
+}[keyof ObjectType & string];
+
 /**
  * Language code type.
  *
